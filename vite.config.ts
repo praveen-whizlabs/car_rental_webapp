d780e66046e7ec1e517a80e445d352ba7b5e9b35
praveen-whizlabs <praveen.whizlabs@gmail.com>
2025-06-28 17:49:21 +0530

    check github actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 👈 This is essential for S3!
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
